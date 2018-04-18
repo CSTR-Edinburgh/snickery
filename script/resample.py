6e792b68aa4bcfdbdb5ebeec49accc8133a0c7fd
Oliver Watts <owatts@staffmail.ed.ac.uk>
2018-04-18 16:55:46 +0100

    add function to pitch sync features; add STRAIGHT window convention

--- script/resample.py
+++ script/resample.py
@@ -50,7 +50,15 @@ def get_mfcc_frame_centres(wavlength, sample_rate, fshift_seconds, flength_secon
 
     return np.array( time_axis)
 
-
+def get_straight_frame_centres(wavlength, sample_rate, fshift_seconds):
+    ## !!!! This is hacked from the world version -- not checked !!!!!!
+    samples_per_frame = int(sample_rate * fshift_seconds)
+    f0_length = (wavlength / float(sample_rate) / fshift_seconds) + 1
+    time_axis = []
+    for i in range(int(f0_length)):
+        time_axis.append(i * samples_per_frame)
+    time_axis = time_axis[:-1] ## omit last value for STRAIGHT...
+    return np.array(time_axis)
 
 def upsample(len_wave, sample_rate, fshift_seconds, features, f0_dim=-1, convention='world'):
 
@@ -58,6 +66,8 @@ def upsample(len_wave, sample_rate, fshift_seconds, features, f0_dim=-1, convent
         frame_centres = get_world_frame_centres(len_wave, sample_rate, fshift_seconds)
     elif convention == 'snack':
         frame_centres = get_snack_frame_centres(len_wave, sample_rate, fshift_seconds)
+    elif convention == 'straight':
+        frame_centres = get_straight_frame_centres(len_wave, sample_rate, fshift_seconds)        
     else:
         sys.exit('Unknown windowing convention: %s'%(convention))
 
@@ -66,12 +76,24 @@ def upsample(len_wave, sample_rate, fshift_seconds, features, f0_dim=-1, convent
     
     diff = len(frame_centres) - m
 
-    if diff not in [-0,1]:
+    if convention == 'straight':
+        tolerance = [-1,0,1]
+    else:
+        tolerance = [-0,1]
+
+    # print 'diff'
+    # print diff
+    
+    if diff not in tolerance:
         print 'Warning: Inconsistent data sizes -- return empty matrix!'
         return np.zeros(0)
 
     if diff == 1:
         frame_centres = frame_centres[:-1]      
+    elif diff == -1:
+        ## repeat last value
+        #frame_centres = np.concatenate([frame_centres, frame_centres[-1:]])
+        features = features[:-1,:]
     
     resampled = np.zeros((len_wave, n))
     for dim in xrange(n):
@@ -79,7 +101,7 @@ def upsample(len_wave, sample_rate, fshift_seconds, features, f0_dim=-1, convent
             y = features[:,dim]
 
             voiced_ix = np.where( y > 0.0 )  ## equiv to np.nonzero(y)
-            unvoiced_ix = np.where( y == 0.0 )
+            unvoiced_ix = np.where( y <= 0.0 )
             
             voicing_flag = np.zeros(y.shape)
             voicing_flag[voiced_ix] = 1.0
@@ -97,7 +119,7 @@ def upsample(len_wave, sample_rate, fshift_seconds, features, f0_dim=-1, convent
             u_interpolated = interpolator(np.arange(len_wave))            
             
             interpolated = v_interpolated # np.minimum(v_interpolated, u_interpolated)
-            interpolated[u_interpolated==0] = 0.0
+            interpolated[u_interpolated==0] = -1000.0
                    
         else:
         
@@ -114,3 +136,54 @@ def upsample(len_wave, sample_rate, fshift_seconds, features, f0_dim=-1, convent
     
 
 
+def pitch_synchronise(speech, streams, stream_widths, sample_rate=48000, fshift_seconds=0.005, uv_length_sec=0.005, convention='straight'):
+    assert 'f0' in streams or 'lf0' in streams, 'need fundamental frequency to pitch synchronise'
+    freq_dim = 0
+    for stream in streams:
+        if stream in ['f0', 'lf0']:
+            freq_stream = stream
+            break
+        freq_dim += stream_widths[stream]
+
+    len_wave = int(speech.shape[0] * fshift_seconds * sample_rate)
+    #upsampled_ffreq = upsample(len_wave, sample_rate, fshift_seconds, ffreq, f0_dim=freq_dim, convention='straight')
+    upsampled_speech = upsample(len_wave, sample_rate, fshift_seconds, speech, f0_dim=freq_dim, convention=convention)
+    upsampled_ffreq = upsampled_speech[:, freq_dim].reshape((-1,1))
+
+    if freq_stream == 'lf0':
+         upsampled_ffreq = np.exp(upsampled_ffreq)
+         upsampled_ffreq[upsampled_ffreq==1.0] = 0.0
+       
+    pms = get_synthetic_pitchmarks(upsampled_ffreq, sample_rate, uv_length_sec)
+
+    # print freq_stream
+    # ffreq = speech[:,freq_dim].reshape((-1,1))
+    # if freq_stream == 'lf0':
+    #     ffreq = np.exp(ffreq)
+    # print ffreq
+    pms = pms.clip(0, upsampled_speech.shape[0]-1) ## protect against out-of-bounds
+    ps_speech = upsampled_speech[pms, :]
+    return ps_speech
+
+def get_synthetic_pitchmarks(fz_per_sample, srate, uv_length_sec):  
+    '''
+    Assume F0 is already upsampled, and uv regions are 0
+    '''
+    uv_length_samples = uv_length_sec * srate
+    ## make pitch marks:
+    current = 0
+    pms = [current]
+    while True:
+        val = int(fz_per_sample[current])
+        if val == 0:
+            current += uv_length_samples
+        else:
+            current += srate / val
+                
+        if current >= len(fz_per_sample):
+            break
+        
+        current = int(current)
+        
+        pms.append(current)
+    return np.array(pms)
